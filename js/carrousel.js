243a148ed570643d2f47d012b0e4cf770bbdd8ea
Delecroix <d-alexis@hotmail.fr>
2022-12-14 13:23:37 +0100

    Tri Carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -25,21 +25,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 5000);
   });
 
-// $('.carousel.carousel-slider').carousel({
-//     fullWidth: true
-//  });
-
-
-//  // move next carousel
-//  $('.moveNextCarousel').click(function(e){
-//     e.preventDefault();
-//     e.stopPropagation();
-//     $('.carousel').carousel('next');
-//  });
-
-//  // move prev carousel
-//  $('.movePrevCarousel').click(function(e){
-//     e.preventDefault();
-//     e.stopPropagation();
-//     $('.carousel').carousel('prev');
-//  });
