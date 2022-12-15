9bef6219ebe465140bc5a233bdcd08c8b3b6cc4d
Delecroix <d-alexis@hotmail.fr>
2022-12-15 16:50:49 +0100

    w3c

--- js/map.js
+++ js/map.js
@@ -7,16 +7,34 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 const urlgpx = "http://195.14.105.123:1337"
-const url = "http://195.14.105.123:1337/api/datagpxes/?populate=*";
+const url = "http://195.14.105.123:1337/api/Gpxes/?populate=*";
 
 fetch(url)
 .then(response => response.json())
 .then(data => {
-  var adresse = urlgpx + data.data[0].attributes.gpx.data.attributes.url;
+  
+
+var nb = data.meta.pagination.total + 1;
+
+for (let i = 0 ; i < nb ; i++) 
+{
+  let adresse = urlgpx + data.data[i].attributes.GPX.data.attributes.url;
+  console.log(adresse)
+
+  let gpxx = adresse;
+
+new L.GPX(gpxx, {
+  gpx_options: {
+    joinTracksSegments: false
+  },
+}).on('loaded', function(e){
+  map.fitBounds(e.target.getBounds());
+})
+.addTo(map);}
+})
 
 
-  var gpx = adresse;
-  console.log(adresse);
+
 // new L.GPX(gpx, {
 //   gpx_options: {
 //     joinTracksSegments: false
@@ -31,13 +49,3 @@ fetch(url)
 //   // console.log(e.target.get_distance(50.9648361,1.8614905 ))
 // }).addTo(map);
 
-
-new L.GPX(gpx, {
-  gpx_options: {
-    joinTracksSegments: false
-  },
-}).on('loaded', function(e){
-  map.fitBounds(e.target.getBounds());
-})
-.addTo(map);
-})
