3b0e5c0182f9c5989e2bfe38ea253837ae039586
Delecroix <d-alexis@hotmail.fr>
2022-12-20 21:13:54 +0100

    w3c

--- js/etapes.js
+++ js/etapes.js
@@ -1,6 +1,6 @@
 
 //code afficher map
-var map = L.map('map').setView([50.7606273570878, 2.2339190414235413], 10);
+var map = L.map('map').setView([50.7606273570878, 2.2339190414235413], 9);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
